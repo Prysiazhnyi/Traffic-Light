71e69cabf541d2b2b074206df310a28eb26aa7f9
sprysiazhnyi <sprysiazhnyi@competo.io>
2025-01-06 17:26:48 +0200

    add-func

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -15,30 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenView: UIView!
     
     var timer: Timer?
+    var remainingSeconds = 10
+    var selectLight = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let trafficLightViews: [UIView] = [redView, yelowView, greenView]
-//        
-//        for view in trafficLightViews {
-//            trafficLoghtView(view)
+        // Массив для удобства обработки
+        let trafficLightViews: [UIView] = [redView, yelowView, greenView]
+        
+        for view in trafficLightViews {
+            trafficLoghtView(view)
         }
         
-        override func viewDidLayoutSubviews() {
-               super.viewDidLayoutSubviews()
-               
-               // Массив для удобства обработки
-               let trafficLightViews: [UIView] = [redView, yelowView, greenView]
-               
-               for view in trafficLightViews {
-                   view.frame.size = CGSize(width: 200, height: 200)
-                   trafficLoghtView(view)
-               }
-           }
+        selectTrafficLight()
         
-
+    }
+    
     @IBAction func actionButtom(_ sender: Any) {
+        selectTrafficLight()
     }
     
     func trafficLoghtView(_ view: UIView) {
@@ -48,6 +43,64 @@ class ViewController: UIViewController {
         view.backgroundColor = .clear
     }
     
-
+    
+    func startTimer() {
+        timer?.invalidate() // Останавливаем предыдущий таймер, если он есть
+        updateTimeLabel() // Обновляем метку сразу
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        if remainingSeconds > 0 {
+            remainingSeconds -= 1 // Уменьшаем время
+            updateTimeLabel() // Обновляем текст метки
+        } else {
+            timer?.invalidate() // Останавливаем таймер
+            remainingSeconds = 10
+            selectTrafficLight()
+            //startTimer() // Снова запускаем таймер
+        }
+    }
+    
+    func updateTimeLabel() {
+        timeLabel.text = String(format: "%02d", remainingSeconds)
+        //timeLabel.textColor = .red
+    }
+    
+    func selectTrafficLight() {
+        
+        selectLight += 1
+        
+        // Сбрасываем все цвета
+            redView.backgroundColor = .clear
+            yelowView.backgroundColor = .clear
+            greenView.backgroundColor = .clear
+        
+        if selectLight > 2 {
+            selectLight = 0
+        }
+        
+        // Включаем свет
+        switch selectLight {
+        case 0:
+            redView.backgroundColor = .red
+            timeLabel.textColor = .red
+            remainingSeconds = 5
+        case 1:
+            yelowView.backgroundColor = .yellow
+            timeLabel.textColor = .yellow
+            remainingSeconds = 3
+        case 2:
+            greenView.backgroundColor = .green
+            timeLabel.textColor = .green
+            remainingSeconds = 8
+        default:
+            break
+        }
+        startTimer()
+        print("Current Light: \(selectLight), time = \(remainingSeconds)")
+       
+    }
+    
 }
 
