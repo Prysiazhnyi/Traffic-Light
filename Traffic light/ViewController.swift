94c4c61cea3ca54026813077a9592a0601953186
sprysiazhnyi <sprysiazhnyi@competo.io>
2025-01-06 15:24:43 +0200

    Initial Commit

---
+++ Traffic light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic light
+//
+//  Created by Serhii Prysiazhnyi on 06.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
