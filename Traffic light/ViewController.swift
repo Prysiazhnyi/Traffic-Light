fd2d0866a4bc72171b9ee52aa01b00213f1c221b
sprysiazhnyi <sprysiazhnyi@competo.io>
2025-01-06 16:23:25 +0200

    add

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yelowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+//        let trafficLightViews: [UIView] = [redView, yelowView, greenView]
+//        
+//        for view in trafficLightViews {
+//            trafficLoghtView(view)
+        }
+        
+        override func viewDidLayoutSubviews() {
+               super.viewDidLayoutSubviews()
+               
+               // Массив для удобства обработки
+               let trafficLightViews: [UIView] = [redView, yelowView, greenView]
+               
+               for view in trafficLightViews {
+                   view.frame.size = CGSize(width: 200, height: 200)
+                   trafficLoghtView(view)
+               }
+           }
+        
 
+    @IBAction func actionButtom(_ sender: Any) {
+    }
+    
+    func trafficLoghtView(_ view: UIView) {
+        view.layer.cornerRadius = view.frame.width / 2
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.black.cgColor
+        view.backgroundColor = .clear
+    }
+    
 
 }
 
